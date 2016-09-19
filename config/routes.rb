0f3441645920f957a92c8e6b6fc747d54369e8e2
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2016-09-19 20:23:14 +0000

    aula 07

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, path: ''
-  resources :employees, path: 'colaboradores' do
+  devise_for :users,  path: 'usuarios', path_names: { sign_in: 'entrar', sign_out: 'sair', password: 'senha', new_user_password: '', confirmation: 'confirmacao', sign_up: 'cadastrar' }
+
+  resources :employees, path: 'colaboradores', path_names: { new: 'cadastrar', edit: 'editar' } do
     resources :contacts, path: 'contatos', path_names: { new: 'cadastrar', edit: 'editar' }
   end
-  #resources :employees, path: 'colaboradores', path_names: { new: 'cadastrar', edit: 'editar' } do
-  #  resources :contacts, path: 'contatos', path_names: { new: 'cadastrar', edit: 'editar' }
-  #end
 
   get 'welcome/index'
 
