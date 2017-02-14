060748c2e83c2c079963e8fc01b5bedd65796de3
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2017-02-14 10:41:56 -0200

    Update smtp_settings.rb

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port:                 '587',
   domain:               'gmail.com',
   user_name:            'rodrigovdbmailer',
-  password:             'rapadura',
+  password:             '********',
   authentication:       'plain',
   enable_starttls_auto: true
 }
